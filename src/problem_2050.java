a16541434f220fe57f8718ea4141232e8c9c1770
yerin <k67065411@gmail.com>
2022-01-16 16:52:42 +0900

    SWEA problem 2050

---
+++ src/problem_2050.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class problem_2050 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String sen = sc.nextLine();
+        char[] alp = sen.toCharArray();
+
+        for(int i=0; i<alp.length; i++){
+            //A = 65
+            System.out.print((int)alp[i] - 64  + " ");
+        }
+    }
+}
