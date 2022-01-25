fd8b1a5953d80a271d27a0177f4c0d4f87f3da9b
ggg06224 <ggg06224@naver.com>
2022-01-25 20:24:05 +0900

    SSFAY study 01.25

---
+++ src/problem_1989.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class problem_1989 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        int cnt = 0;
+
+        for(int tc = 0; tc<t; tc++){
+            cnt = 0;
+            String word = sc.next();
+            sc.nextLine();
+            String[] alp = word.split("");
+
+            for(int i=0; i<alp.length/2; i++){
+                //System.out.println(alp[i] + " " + alp[alp.length-i-1]);
+                if(!alp[i].equals(alp[alp.length-i-1])){
+                    System.out.printf("#%d 0\n",tc+1);
+                    break;
+                }
+                cnt++;
+            }
+            if(cnt == alp.length/2)
+                System.out.printf("#%d 1\n",tc+1);
+        }
+
+    }
+}
