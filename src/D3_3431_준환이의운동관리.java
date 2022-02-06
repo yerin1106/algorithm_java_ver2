9f0d6118b14906ebd9c14ed8d22ae562d7d0c35e
ggg06224 <ggg06224@naver.com>
2022-02-06 16:47:03 +0900

    SSFAY study 02.06

---
+++ src/D3_3431_준환이의운동관리.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+// 10조_심재서_김예린
+public class D3_3431_준환이의운동관리 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        for(int i=0; i<t; i++){
+            int l = sc.nextInt();
+            int u = sc.nextInt();
+            int x = sc.nextInt();
+
+            if(x < l) System.out.printf("#%d %d\n", i+1, l-x);
+            else if(x>u) System.out.printf("#%d -1\n", i+1);
+            else System.out.printf("#%d 0\n", i+1);
+        }
+    }
+}
