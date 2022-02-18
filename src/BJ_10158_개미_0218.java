d607d4ea42e28e0396ba01d0ffa2b3d21bbfd9f3
ggg06224 <ggg06224@naver.com>
2022-02-18 20:37:59 +0900

    SSAFY study 02.18

---
+++ src/BJ_10158_개미_0218.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_10158_개미_0218 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int p = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+
+        int t = Integer.parseInt(br.readLine());
+
+        int a = w - Math.abs(w - (p+t) % (2*w));
+        int b = h - Math.abs(h - (q+t) % (2*h));
+
+        System.out.println(a + " " + b);
+    }
+}
