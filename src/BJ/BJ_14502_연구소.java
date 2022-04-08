b667192d6a0bc38c66e621c6391ded75ce7e1732
ggg06224 <ggg06224@naver.com>
2022-04-08 10:09:02 +0900

    SSAFY course 04.08

--- src/BJ/BJ_14502_연구소.java
+++ src/BJ/BJ_14502_연구소.java
@@ -1,12 +1,9 @@
 package BJ;
 
-import com.sun.xml.internal.ws.encoding.soap.SOAP12Constants;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BJ_14502_연구소 {
